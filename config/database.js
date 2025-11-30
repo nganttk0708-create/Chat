c69d17c701e6887f68549e7022a0f372ca1d68f2
Ngân Kiều <nganttk0708@ut.edu.vn>
2025-12-01 02:01:39 +0700

    Refactor MongoDB connection handling
    Updated MongoDB connection to use only environment variable.

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 module.exports.connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/chat');
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('Connect success!');
   } catch (error) {
-    console.log('Connect error!');
+    console.log('Connect error!', error.message);
   }
 };
