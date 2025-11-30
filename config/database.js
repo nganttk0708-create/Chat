84d16c0bd0347a67e36c712be564c191275d86bb
Ngân Kiều <nganttk0708@ut.edu.vn>
2025-12-01 01:40:39 +0700

    Update MongoDB connection string to use Atlas

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 module.exports.connect = async () =>{
     try{
-        await mongoose.connect('mongodb://127.0.0.1:27017/Chat')
+        await mongoose.connect('mongodb+srv://chatweb:ngan.123@ngan.2fxcirz.mongodb.net/chat')
         console.log('Connect success!')
     }catch(error){
         console.log('Connect error!')
